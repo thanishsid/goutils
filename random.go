f0351229e1ba0f3dc4fa420a9c030568a95d2ad9
Thanish Siddique <thanish@Thanishs-MacBook-Pro.local>
2023-10-05 08:59:44 +0530

    added charset arg to create random string function

--- random.go
+++ random.go
@@ -2,18 +2,30 @@ package goutils
 
 import (
 	"math/rand"
+	"strings"
 	"time"
 )
 
 var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
-var randCharset = "abcdefghijklmnopqrstuvwxyz" + "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + numericCharset
-var numericCharset = "0123456789"
 
-// Create random string with an optional function to check for randomness if not returned true will recursively generate new random string
-func CreateRandomString(strLength int, checkFunc func(str string) bool) string {
+const AlphabeticCharsetLower = "abcdefghijklmnopqrstuvwxyz"
+const AlphabeticCharsetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const NumericCharset = "0123456789"
+
+var defaultCharset = AlphabeticCharsetLower + AlphabeticCharsetUpper + NumericCharset
+
+// Create random string with an optional function to check for randomness if not returned true will recursively generate new random string.
+// Optionally provide charsets to use when generating the random string. If no charsets are provided then a random alphanumeric string will be generated.
+func CreateRandomString(strLength int, checkFunc func(str string) bool, charsets ...string) string {
+	charset := strings.Join(charsets, "")
+
+	if charset == "" {
+		charset = defaultCharset
+	}
+
 	b := make([]byte, strLength)
 	for i := range b {
-		b[i] = randCharset[seededRand.Intn(len(randCharset))]
+		b[i] = charset[seededRand.Intn(len(charset))]
 	}
 
 	randStr := string(b)
@@ -25,11 +37,11 @@ func CreateRandomString(strLength int, checkFunc func(str string) bool) string {
 	return CreateRandomString(strLength, checkFunc)
 }
 
-// Create random numeric string with an optional function to check for randomness if not returned true will recursively generate new random numeric string
+// Create random numeric string with an optional function to check for randomness if not returned true will recursively generate new random numeric string.
 func CreateRandomNumericString(strLength int, checkFunc func(str string) bool) string {
 	b := make([]byte, strLength)
 	for i := range b {
-		b[i] = numericCharset[seededRand.Intn(len(numericCharset))]
+		b[i] = NumericCharset[seededRand.Intn(len(NumericCharset))]
 	}
 
 	randStr := string(b)
--- random_test.go
+++ random_test.go
@@ -6,6 +6,8 @@ func TestCreateRandomString(t *testing.T) {
 	t.Parallel()
 	str := CreateRandomString(6, func(str string) bool { return true })
 
+	t.Logf("\nrandom string: %s\n", str)
+
 	if len(str) != 6 {
 		t.Error("random string incorrect length")
 	}
